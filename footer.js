1f55f881b0d75cffeb497160df20466d7246a6d0
geraud82 <137027447+geraud82@users.noreply.github.com>
2023-08-15 15:18:25 -0500

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,25 @@
+const appFooter = `<section class="foot">
+<div  class="footer">
+  <div class="footer1">© KADNO SA -2023. All rights reserved. Designed by Neobize 
+
+      The purpose of this website is solely to display information regarding all services KADNO SA PROVIDE.
+    </div>
+</div>
+<div class="boss-foote">
+<div class=" foote">
+<ul>
+<li>  <a  class="lolo"  href="">Vie Privee</a></li>
+<li><a class="lolo" href="">Politique d'utilisateur</a></li>
+</ul>
+</div>
+<div class=" foote1">
+
+<ul>
+<li>  <a  class="lolo" href="">FaceBook</a></li>
+<li><a class="lolo"      href="">YouTube</a></li>
+</ul>
+</div>
+</div>
+
+</section> `;
+document.getElementById("footerApp").innerHTML = appFooter;
