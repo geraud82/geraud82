ee4984ccc62f0bfe26cbf33ac35950131986d142
geraud82 <137027447+geraud82@users.noreply.github.com>
2023-08-21 20:22:02 -0500

    Add files via upload

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 const appHeader =`<div class="container-fluid">
 <nav class="navbar navbar-expand-lg bg-primaary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img  class="logokadnosa"  src="kadnologo.jpeg"> </a>
+    <a class="navbar-brand" href="#"><img  class="logokadnosa"  src="kadnologo.png"> </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
